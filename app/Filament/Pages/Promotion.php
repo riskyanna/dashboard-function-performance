140b6be20510604c27ac9934bbcfbde72532d912
Riski Ramadhan <riskycas23@gmail.com>
2026-01-15 14:20:18 +0700

    Transform Promotion page to Developer Profile with JTEKT acknowledgement

--- app/Filament/Pages/Promotion.php
+++ app/Filament/Pages/Promotion.php
@@ -8,9 +8,9 @@ class Promotion extends Page
 {
     protected string $view = 'filament.pages.promotion';
     
-    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-megaphone';
-    protected static ?string $navigationLabel = 'Promosi Jasa';
-    protected static ?string $title = 'Promosi Jasa Web';
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-user-circle';
+    protected static ?string $navigationLabel = 'Profile Developer';
+    protected static ?string $title = 'Tentang Pengembang';
     protected static string | \UnitEnum | null $navigationGroup = 'Tentang Developer';
     protected static ?int $navigationSort = 1;
 }
